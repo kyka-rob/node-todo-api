5a56986ab6f5d580e3640a5cc44337dcab71b9a5
kyka_rob <saroj38508@gmail.com>
2017-11-27 16:14:42 +0700

    update config.js for test & dev environment only

--- server/config/config.js
+++ server/config/config.js
@@ -3,8 +3,8 @@ var env = process.env.NODE_ENV || 'development'
 if(env === 'test' || env === 'development') {
     var config = require('./config.json')
     var configEnv = config[env]
+    
+    Object.keys(configEnv).forEach((key) => {
+        process.env[key] = configEnv[key]
+    })
 }
-
-Object.keys(configEnv).forEach((key) => {
-    process.env[key] = configEnv[key]
-})
