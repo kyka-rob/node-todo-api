44cf3f2dcb49741ed04a9f12b4b0c933387e38da
kyka_rob <saroj38508@gmail.com>
2017-11-27 16:12:51 +0700

    update config.js

--- server/config/config.js
+++ server/config/config.js
@@ -1,7 +1,9 @@
 var env = process.env.NODE_ENV || 'development'
 
-var config = require('./config.json')
-var configEnv = config[env]
+if(env === 'test' || env === 'development') {
+    var config = require('./config.json')
+    var configEnv = config[env]
+}
 
 Object.keys(configEnv).forEach((key) => {
     process.env[key] = configEnv[key]
